a3f1d11fefc761766bd151fffda15035d55aa1dc
Aarya Paradkar <aaru7276@gmail.com>
2023-09-02 12:38:08 +0530

    added github path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo.svg';
 
 const Hero = () => {
   return (
     <header className='w-full flex center items-center flex-col'>
-      <nav className='flex justify-between items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        {/* img */}
         <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
+
+        {/* button */}
+        <button type='button' onClick={() => window.open('https://github.com/aaryaparadkar')} className='black_btn'>Github</button>
       </nav>
     </header>
   )
