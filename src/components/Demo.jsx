059846c83792811909b01e3934f12a206c55c16c
Aarya Paradkar <aaru7276@gmail.com>
2023-09-02 13:22:24 +0530

    created the search box

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
+const [article, setarticle] = useState({
+  url: '',
+  summary: '',
+});
+
+const handleSubmit = async (e) => {
+  alert('Submitted');
+}
+
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+
+      {/* search box */}
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+          <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
+          <input type="url" placeholder="Enter the url" value={article.url} onChange={(e) => setarticle({ ...article, url: e.target.value })} required className="url_input peer" />
+          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-700">
+          ↵
+          </button>
+        </form>
+
+        {/* url history */}
+      </div>
+
+      {/* result */}
+    </section>
   )
 }
 
