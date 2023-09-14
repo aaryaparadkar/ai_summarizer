43780d44aaaac0b7a7ce13307999ac4992ad746c
Aarya Paradkar <aaru7276@gmail.com>
2023-09-14 22:51:12 +0530

    minor ui change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/logo.svg";
 
 const Hero = () => {
   return (
-    <header className='w-full flex center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+    <header className='w-full items-center'>
+      <nav className='flex justify-evenly items-center w-full pt-3' style={{ height: "400px" }}>
         {/* img */}
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
