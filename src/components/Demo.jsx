b085024765e9ebc8e5c9c7e36d2be1ceaf59b07f
Aarya Paradkar <aaru7276@gmail.com>
2023-09-14 23:39:45 +0530

    finally done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const [article, setArticle] = useState({
   summary: '',
 });
 const [allArticles, setAllArticles] = useState([]);
+const [copied, setCopied] = useState(second);
 
 const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -38,6 +39,12 @@ e.preventDefault();
   }
 }
 
+  const handleCopy = (copyUrl => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  })
+
   return (
     <section className="mt-16 w-full max-w-xl">
 
@@ -55,8 +62,8 @@ e.preventDefault();
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.map((item, index) => {
             <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
-              <div className="copy_btn">
-                <img src={copy} alt="copy_icon" className="w-[40%} h-[40%]} contain" />
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img src={copied === item.url ? tick: copy} alt="copy_icon" className="w-[40%} h-[40%]} contain" />
               </div>
               <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
             </div>
@@ -81,7 +88,7 @@ e.preventDefault();
             <div className="glex flex-col gap-3">
               <h2 className="font-satoshi font-bold text-gray-600 text-xl">Article <span className="blue-gradient">Summary</span></h2>
               <div className="summary_box">
-                <p>{article.summary}</p>
+                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
               </div>
             </div>
           )
